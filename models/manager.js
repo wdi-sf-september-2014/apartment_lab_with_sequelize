bbb1690e08cce738d248f61d07f075e88c244b72
Alex Notov <anotov@gmail.com>
2014-10-21 06:00:33 -0700

    Better error messages

--- models/manager.js
+++ models/manager.js
@@ -5,13 +5,17 @@ module.exports = function(sequelize, DataTypes) {
     firstname: {
       type: DataTypes.STRING,
       validate: {
-        isAlpha: true
+        isAlpha: {
+          msg: 'Your first name cannot have anything except letters'
+        }
       }
     },
     lastname: {
       type: DataTypes.STRING,
       validate: {
-        isAlpha: true
+        isAlpha: {
+          msg: 'Your last name cannot have anything except letters'
+        } 
       }
     },
     property: DataTypes.STRING
